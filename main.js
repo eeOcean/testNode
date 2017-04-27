b075ce7f46d1a62bfb6bd366331e1de43f37e0c0
Egor Erygin <ocean@MacBook-Pro-Egor.local>
2017-04-27 22:35:59 +0300

    Start learning git

---
+++ main.js
@@ -0,0 +1,15 @@
+var express = require('express'),
+	app = express();
+
+var page = {
+	page: "This in main page",
+	content: "Lorem ipsum dolor sit amet, consectetur adipisicing elit. Aliquam, atque."
+}
+
+app.get("/", function(req, res){
+	res.render("main.jade", page);
+});
+
+app.listen(3000, function(){
+	console.log("Wroking on port 3000");
+});
